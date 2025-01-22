b4b12f23c3ee15b5ebd3ea25b0b6984d6836e691
taniarmhm <mejiatt56@gmail.com>
2025-01-22 14:43:49 -0600

    estructura de un archivo  java y mensaje de cadenas

--- Sintaxis_2/src/Main.java
+++ Sintaxis_2/src/Main.java
@@ -1,13 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//clase principal en java
 public class Main {
+    //metodo main
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+//        2. Cadenas con Java
         System.out.printf("HELLO WORLD");
-        System.out.printf("HELLO TANIA ");
+        System.out.printf("HELLO, TANIA ");
+        String cadenas = "tania" + "mejia" + "moreno";
 
-
+        System.out.println(cadenas);
+        System.out.println(cadenas.length());
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(2));
+        System.out.println(cadenas.substring(0,5));
     }
 
 }
