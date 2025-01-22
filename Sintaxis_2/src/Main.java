37908ebac2ee34e8164c57797999f8da37f387ac
taniarmhm <mejiatt56@gmail.com>
2025-01-22 15:35:18 -0600

    finalizamos practica n2 sintaxis

--- Sintaxis_2/src/Main.java
+++ Sintaxis_2/src/Main.java
@@ -1,18 +1,95 @@
+import java.util.Random;
 
 //clase principal en java
-public class Main {
-    //metodo main
-    public static void main(String[] args) {
-//        2. Cadenas con Java
-        System.out.printf("HELLO WORLD");
-        System.out.printf("HELLO, TANIA ");
-        String cadenas = "tania" + "mejia" + "moreno";
+// 1iportaciones
+ import java.util.Random;
+import java.util.Scanner;
 
-        System.out.println(cadenas);
-        System.out.println(cadenas.length());
-        System.out.println(cadenas.substring(2,5));
-        System.out.println(cadenas.substring(2));
-        System.out.println(cadenas.substring(0,5));
+public class Main {
+ //metodo main
+    public static void main(String[] args) {
+//      2. Cadenas con Java
+//        System.out.printf("HELLO WORLD");
+//        System.out.printf("HELLO, TANIA ");
+//        String cadenas = "tania" + "mejia" + "moreno";
+//
+//        System.out.println(cadenas);
+//        System.out.println(cadenas.length());
+//        System.out.println(cadenas.substring(2, 5));
+//        System.out.println(cadenas.substring(2));
+//        System.out.println(cadenas.substring(0, 5));
+//
+//
+//        3. Variables
+//
+//        int x = 5, x2;
+//        double y = 1.25, y2;
+//        String z = "204", z1;
+//    //converion de cadena a int
+//        x2= Integer.parseInt(z);
+//        //conversion de double a cadena
+//        z1= String.valueOf(y);
+//        //converion de int a double
+//        double asd= Double.valueOf(x);
+//
+//
+//
+//        System.out.println(x2);
+//        System.out.println(z1);
+//        System.out.println(asd);
+// creamos una clase rdn de Random
+//        Random rdn= new Random();
+//
+//
+//       //generamos numeros y los guardamos
+//        int numAleatorio= rdn.nextInt();
+//        double numDouble= rdn.nextDouble();
+//        System.out.println("Aleatorio entero:" +numAleatorio);
+//        System.out.println("Aletorio double" + numDouble);
+
+
+
+        // 4.SOLICITUD DE DATOS
+//        Scanner scn=new Scanner(System.in);
+//
+//        System.out.println("introdusca cualquier dato");
+//        String dato= scn.nextLine();
+//
+//        System.out.println("introdusca dato entero");
+//        int datoentero= scn.nextInt();
+//
+//        System.out.println("introdusca dato con decimales");
+//        double datocondecimales= scn.nextDouble();
+//
+//
+//
+//
+
+        // 5, Boolean, operadores logicos y de comparacion
+        System.out.println(10 >  9); //true
+        System.out.println(10 == 9); //false
+        System.out.println(10 <  9); //false
+        System.out.println(10 >= 9); //true
+        System.out.println(10 <= 9); //false
+        System.out.println(10 != 9); //true
+
+
+      int  x= 3;
+      System.out.println(x< 5 && x>10);
+        System.out.println(x< 5 || x>10);
+        System.out.println(!(x< 5 && x>10));
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 }
