df3bbed348251bea00351472b3f421151b82d35f
taniarmhm <mejiatt56@gmail.com>
2025-01-28 16:08:40 -0600

    hice el primer ejercisio de la practica 4

---
+++ practica 2/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String contrasenaGuardada = "MiContraseña123";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce la contraseña: ");
+        String contrasenaUsuario = sc.nextLine();
+        if (contrasenaGuardada.equalsIgnoreCase(contrasenaUsuario)) {
+            System.out.println("Contraseña correcta.");
+        } else {
+            System.out.println("Contraseña incorrecta.");
+        }
+
+
+    }
+}
---
+++ practica 2/src/practica2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class practica2 {
+    public static void main(String[] args) {
+        String contrasenaGuardada = "MiContraseña123";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce la contraseña: ");
+        String contrasenaUsuario = sc.nextLine();
+        if (contrasenaGuardada.equalsIgnoreCase(contrasenaUsuario)) {
+            System.out.println("Contraseña correcta.");
+        } else {
+            System.out.println("Contraseña incorrecta.");
+        }
+    }
+}
