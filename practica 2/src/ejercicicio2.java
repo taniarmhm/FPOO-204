dc68963c9a61e130b69b730328b5d6614dc3c614
taniarmhm <mejiatt56@gmail.com>
2025-01-28 16:12:57 -0600

    se realizo el programa un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.

--- practica 2/src/ejercicicio2.java
+++ practica 2/src/ejercicicio2.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
 public class ejercicicio2 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce un número entero: ");
+        int numero = sc.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("El número es par.");
+        } else {
+            System.out.println("El número es impar.");
+        }
+    }
 }
+
